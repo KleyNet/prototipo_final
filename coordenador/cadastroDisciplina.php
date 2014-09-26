a6392b18f67e8418ee6c0e9a75a2895fbe9c361c
Cleynaldo <cleynaldocarvalho@gmail.com>
2014-09-26 02:28:13 -0300

    Alterações do css nas pagginas

--- coordenador/cadastroDisciplina.php
+++ coordenador/cadastroDisciplina.php
@@ -40,6 +40,8 @@
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <?php echo $_SESSION['perfil']; ?> <span class="caret"></span></a>
 						<ul class="dropdown-menu">
+                                                    <li><a href="#"><span class="glyphicon glyphicon-cog"></span> Editar Perfil</a></li>
+							<li class="divider"></li>
 							<li><a href="../logado.php?logout=acessar">Sair <span class="glyphicon glyphicon-log-out"></span></a></li>
 						</ul>
 					</li>
